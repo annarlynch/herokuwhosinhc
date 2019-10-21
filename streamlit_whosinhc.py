28851f86813b0353a7d56d01663f10da1906f12a
annarlynch <46689009+annarlynch@users.noreply.github.com>
2019-10-21 10:57:05 -0500

    Create streamlit_whosinhc.py

---
+++ streamlit_whosinhc.py
@@ -0,0 +1,62 @@
+import streamlit as st
+# To make things easier later, we're also importing numpy and pandas for
+# working with sample data.
+import numpy as np
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+from pandas import DataFrame
+
+"""
+# Hardin County Jail
+Here's our first attempt at using data to create a table:
+"""
+
+def scrape():
+    url = 'http://209.152.119.10/vine/currentinmates/currentinmates.html'
+    response = requests.get(url)
+    html = response.content
+    soup = BeautifulSoup(html, "lxml")
+    rows = soup.find_all('tr')
+    prisoner_rows = rows[1:]
+    ar = np.array(prisoner_rows)
+    prisoner_chunks = np.split(ar, len(prisoner_rows) / 6)
+
+    def chunkdict(chunk):
+        output = {}
+        output["pic"] = chunk[0].find_all("img")[0]["src"]
+        output["name"] = chunk[0].find_all("td")[-1].text
+        output["age"] = chunk[1].find_all("td")[-1].text
+        output["gender"] = chunk[2].find_all("td")[-1].text
+        output["detained"] = chunk[3].find_all("td")[-1].text
+        output["released"] = chunk[4].find_all("td")[-1].text
+        output["charges"] = chunk[5].find_all("td")[-1].text
+        return output
+    prisoner_dicts = [chunkdict(x) for x in prisoner_chunks]
+    df = pd.DataFrame(prisoner_dicts)
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    df = df[['name', 'gender', 'age', 'detained', 'charges', 'released', 'pic']]
+    df_filtered = df[df['charges'].str.contains('ICE ', na = False)]
+    df_filtered.insert(loc=0, column='Row Number', value=np.arange(len(df_filtered)))
+
+    return(df_filtered)
+
+    scrape()
+
+df = pd.DataFrame(scrape())
+df
+
+st.subheader("How many people are currently detained?")
+#Get a bool series representing which rows satisfy the condition of True for no
+# date in released column
+how_many = df.apply(lambda x: True if x['released'] == "-" else False, axis =1)
+# Count number of True in series
+numOfRows = len(how_many[how_many == True].index)
+st.write("Number of people currently detained: ", numOfRows)
+
+released = st.sidebar.multiselect('Create a table belowing showing only detainees with selected release dates:', df['released'].unique())
+
+st.subheader("Use the search bar on the left to show detaines with a selected release date")
+new_df = df[(df['released'].isin(released))]
+st.write(new_df)
